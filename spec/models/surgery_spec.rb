07891baeaa09565658a0a98e14db2e768d2b5b37
Dani Coleman <danicoleman00@gmail.com>
2020-09-22 11:41:23 -0600

    Add test for model method

--- spec/models/surgery_spec.rb
+++ spec/models/surgery_spec.rb
@@ -10,5 +10,19 @@ RSpec.describe Surgery, type: :model do
     it { should validate_presence_of :title }
     it { should validate_presence_of :day }
     it { should validate_presence_of :room }
-  end 
+  end
+
+  describe 'instance methods' do
+    it "can get same day surgeries" do
+      hosp1 = Hospital.create!(name:"Grey Sloan Memorial Hospital")
+      doc1  = hosp1.doctors.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University")
+      doc2  = hosp1.doctors.create!(name: "Alex Karev", specialty: "Pediatric Surgery", university: "Johns Hopkins University")
+      surg1 = doc1.surgeries.create!(title: "Knee", day: "Monday", room:"1")
+      surg3 = doc2.surgeries.create!(title: "Shoulder", day: "Monday", room:"3")
+
+      expected = ["Shoulder"]
+
+      expect(surg1.day_of_surgery).to eq(expected)
+    end
+  end
 end
